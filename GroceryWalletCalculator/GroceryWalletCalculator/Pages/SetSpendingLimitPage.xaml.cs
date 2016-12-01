bfffc772a6233bc50bfdb131b0c3785272262a10
Matt Hoffman <hofmadresu@gmail.com>
2016-11-30 23:21:30 -0500

    add placeholder view and initial vm for set spending limit

---
+++ GroceryWalletCalculator/GroceryWalletCalculator/Pages/SetSpendingLimitPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroceryWalletCalculator.ViewModels;
+using Xamarin.Forms;
+
+namespace GroceryWalletCalculator.Pages
+{
+    public partial class SetSpendingLimitPage : ContentPage
+    {
+        public SetSpendingLimitPage(int storeId)
+        {
+            InitializeComponent();
+            BindingContext = new SetSpendingLimitViewModel(storeId);
+        }
+    }
+}
---
+++ GroceryWalletCalculator/GroceryWalletCalculator/ViewModels/SetSpendingLimitViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroceryWalletCalculator.Persistence;
+using MvvmHelpers;
+
+namespace GroceryWalletCalculator.ViewModels
+{
+    public class SetSpendingLimitViewModel : BaseViewModel
+    {
+        private int _storeId;
+        public SetSpendingLimitViewModel(int storeId)
+        {
+            _storeId = storeId;
+            SpendingText = $"How much can you spend at {Data.Stores.Single(s => s.Id == storeId).Name}";
+        }
+
+        private double _spendingLimit;
+        public double SpendingLimit
+        {
+            get { return _spendingLimit; }
+            set { SetProperty(ref _spendingLimit, value); }
+        }
+
+        private string _spendingText;
+        public string SpendingText
+        {
+            get { return _spendingText; }
+            set { SetProperty(ref _spendingText, value); }
+        }
+    }
+}
